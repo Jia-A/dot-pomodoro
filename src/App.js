4fd02a3102a35ac871f7dc6469b6efb9d3e320f7
Jia-A <agrawaljiya2011@gmail.com>
2022-07-01 18:38:52 +0530

    feat : added-dark-theme

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import { Route, Routes } from "react-router-dom"
 import { Landing } from './pages/landing';
 import { Homepage } from './pages/homepage';
 import { Clock } from './pages/clock';
+import { useTheme } from './context/theme-context';
 
 function App() {
+  const { theme } = useTheme();
   return (
-    <div className="App">
+    <div className={`App ${theme === "dark" ? "dark-theme" : ""}`}>
       <Routes>
       <Route path = '/' element = { <Landing/>} />
       <Route path = '/homepage' element = { <Homepage/>} />
