613cf1cd7c9b8e682ba9f3ef7284824d14a763cb
Jia-A <agrawaljiya2011@gmail.com>
2022-07-01 17:03:38 +0530

    fix : removd unnecessary-routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import './App.css';
-import { Login } from './pages/login';
 import { Route, Routes } from "react-router-dom"
-import { Signup } from './pages/signup';
 import { Landing } from './pages/landing';
 import { Homepage } from './pages/homepage';
 import { Clock } from './pages/clock';
@@ -12,9 +10,6 @@ function App() {
       <Routes>
       <Route path = '/' element = { <Landing/>} />
       <Route path = '/homepage' element = { <Homepage/>} />
-      <Route path = "/login" element = { <Login/> } />
-      <Route path = '/signup' element = { <Signup/>} />
-      {/* <Route path = '/clock' element = { <Clock/>} /> */}
       <Route path = '/clock/:taskID' element = { <Clock/>} />
       </Routes>
     </div>
